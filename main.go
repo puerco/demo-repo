38c89f0d68666dbf343ad1f23f2ec7675da8278f
Adolfo Garcia Veytia (puerco) <adolfo.garcia@uservers.net>
2025-01-31 05:51:22 +0100

    Add complex codebase 🔥
    Signed-off-by: Adolfo Garcia Veytia (puerco) <adolfo.garcia@uservers.net>

---
+++ main.go
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.Info("Sending world greetings")
+	fmt.Println("Hello World!")
+}
